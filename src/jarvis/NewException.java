fa9760a09b2f7c6fb424cf682306f4a26171e946
csaez30 <43256308+csaez30@users.noreply.github.com>
2024-03-13 10:50:29 -0300

    Create NewException.java
    cree clase excepcion

---
+++ src/jarvis/NewException.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package jarvis;
+
+/**
+ *
+ * @author csaez
+ */
+public class NewException {
+
+    /**
+     * Creates a new instance of <code>NewException</code> without detail
+     * message.
+     */
+    public NewException() {
+    }
+
+    /**
+     * Constructs an instance of <code>NewException</code> with the specified
+     * detail message.
+     *
+     * @param msg the detail message.
+     */
+  
+}
