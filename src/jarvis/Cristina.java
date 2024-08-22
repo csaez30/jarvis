91cd05ee8765e418433cfe3fca33d9fe0decae20
csaez30 <43256308+csaez30@users.noreply.github.com>
2024-08-22 17:57:15 -0300

    Create Cristina.java
    creee cris

---
+++ src/jarvis/Cristina.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jarvis;
+
+/**
+ *
+ * @author csaez
+ */
+public class Cristina {
+    
+    String caracteristina;
+
+    public Cristina(String caracteristina) {
+        this.caracteristina = caracteristina;
+        System.out.println("es bella");
+    }
+    
+    
+}
